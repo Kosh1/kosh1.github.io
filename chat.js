3905f45ee1cd4aeede92a5b2a8898fbf1458ac2b
Kosh1 <koshmanavt1@gmail.com>
2023-07-11 16:17:11 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,18 +2,6 @@ const form = document.getElementById('inputArea');
 const input = document.getElementById('userInput');
 const chatbox = document.getElementById('chatbox');
 
-window.addEventListener('load', async function(event) {
-    // Initialize the conversation with the system message
-    const initResponse = await fetch('http://localhost:3000/init');
-    const initData = await initResponse.json();
-    const initMessage = initData.message;
-
-    // Create a new paragraph element and add the bot's initial message
-    const initPara = document.createElement('p');
-    initPara.textContent = `Bot: ${initMessage}`;
-    chatbox.appendChild(initPara);
-});
-
 input.addEventListener('keydown', async function(event) {
     // Check if the Enter key was pressed
     if (event.key === 'Enter') {
@@ -41,14 +29,14 @@ input.addEventListener('keydown', async function(event) {
 });
 
 async function generateResponse(message) {
-    const response = await fetch('http://localhost:3000/chat', {
+    const response = await fetch('http://localhost:5000/check_answer', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: message }),
+        body: JSON.stringify({ 'problem': 'Which is greater: 450*65% or 650*45%?', 'answer': message }),
     });
 
     const data = await response.json();
-    return data.message;
+    return data.correct ? "Correct answer!" : "Incorrect, please try again.";
 }
